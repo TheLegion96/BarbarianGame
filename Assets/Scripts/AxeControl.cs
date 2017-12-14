628861495a5a06d9129c94175a0b6441d4122ac3
Andrea tonnini <at.rsm@hotmail.it>
2017-12-15 00:25:38 +0100

    1St Commit
    Started Prototyping Game
    Added Jump Function, Axe Thrown, Axe Destruction

---
+++ Assets/Scripts/AxeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public int axeSpeed=15;
+	// Update is called once per frame
+	void Update () {
+        this.transform.DORotate(Vector3.zero, 60);
+        this.transform.position += new Vector3(axeSpeed,0)*Time.deltaTime;
+	}
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public float jumpForce;
+    public bool jumpAllow=false;
+    public GameObject Axe;
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)&&jumpAllow==true)
+        {
+            transform.position += new Vector3(0, jumpForce);
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Instantiate(Axe);
+            Axe.transform.position = this.transform.GetChild(0).position + transform.right;
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Ground")
+        {
+            jumpAllow = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpAllow = false;
+        }
+    }
+}
